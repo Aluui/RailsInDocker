854cce83a476ba0156908befe168ef13832e40fa
Chibuzor Alumba <coolchibs@gmail.com>
2019-09-20 01:51:34 -0500

    scaffolded out a new entity

---
+++ use_rails.sh
@@ -0,0 +1,6 @@
+# docker-compose run --rm web rails runner 'Joke.create(body: "Knock! Knock! Whos there? Owls say. Owls say who? Yes, they do.")'
+
+# docker-compose run --rm web rails db:create db:migrate
+# run all new migrations: docker-compose run --rm web rails db:migrate
+
+# docker-compose run --rm web rails generate scaffold post title:string body:text
